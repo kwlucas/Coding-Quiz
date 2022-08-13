f71f97ce9dce6e4048b2b18f5eba2358d5811dfe
kwlucas <lucaswkyle@gmail.com>
2022-08-13 13:37:30 -0600

    Psudo code

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+//Start button handler
+
+//Function to set layout for before quiz.
+//Function to set layout for during the quiz.
+//Fucntion to set layout for viewing high scores.
+    //clear local storage when clear scores button is pressed.
+
+//Start timer fuction
+
+//Function to take correct and incorrect answers and place buttons for each one on the screen.
+    //randomize the order of the buttons
+
+//Function for when an answer is pressed. Value assigned to button indicates right or wrong answer. Adjust score accordingly
+
+//When out of time OR all questions answered end game function
+
+//end game function makes layout for entering score.
+
+//Input handler for initals/name
+    //add name and score to high score list
+    //Save score and name to local storage
+
+//on load retrieve local storage scores
